b2b3872055f9983227de2f7727288dc73ce16020
Bahdtoluwani <oladosu.toluene@gmail.com>
2024-05-22 02:49:58 +0100

    Modified Entities

--- Library-Management-System/src/main/java/com/example/Library/Management/System/model/Patron.java
+++ Library-Management-System/src/main/java/com/example/Library/Management/System/model/Patron.java
@@ -12,5 +12,31 @@ public class Patron {
     private Long id;
     private String name;
     private String contactInfo;
+
+    //Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 }
 
