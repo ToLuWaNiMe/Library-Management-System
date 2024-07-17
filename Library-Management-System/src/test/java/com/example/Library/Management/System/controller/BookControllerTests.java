e7d772706159dee80405fd8a2a5c5a2dd1273514
Bahdtoluwani <oladosu.toluene@gmail.com>
2024-07-17 20:56:07 +0100

    Modified Changes

--- Library-Management-System/src/test/java/com/example/Library/Management/System/controller/BookControllerTests.java
+++ Library-Management-System/src/test/java/com/example/Library/Management/System/controller/BookControllerTests.java
@@ -4,14 +4,14 @@ import com.example.Library.Management.System.model.Book;
 import com.example.Library.Management.System.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser(username="user", password="password", roles="USER")
@@ -116,6 +116,6 @@ public class BookControllerTests {
         doNothing().when(bookService).deleteBook(bookId);
 
         mockMvc.perform(delete("/api/books/{id}", bookId))
-                .andExpect(status().isOk());
+                 .andExpect(status().isNoContent());
     }
 }
